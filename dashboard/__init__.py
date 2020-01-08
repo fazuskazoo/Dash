d9e9575290cfb6686ec566def7dbc74ade2c6a88
David Harris <davidharrisnet@gmail.com>
2020-01-07 21:06:27 -0500

    bootstrap jquery

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -1,11 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Dashboard!'
-
-
-return app
-
+    return render_template("/dashboard/index.html")
---
+++ start.py
@@ -0,0 +1,7 @@
+
+from dashboard import app
+
+if __name__ =='__main__':
+
+
+    app.run(port="5002")
