4cd5685f16fce32abdef1f08544d5aced2448a61
David Harris <dharris@2328.lan>
2020-01-14 13:11:36 -0500

    tree grid mess

--- dashboard/views/dashboard.py
+++ dashboard/views/dashboard.py
@@ -7,3 +7,8 @@ bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 @bp.route('/tables')
 def tables():
     return render_template("/dashboard/tables.html")
+
+
+@bp.route('/treegrid')
+def treegrid():
+    return render_template("/dashboard/treegrid.html")
