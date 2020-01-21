e8cb6864e78e21d27e6eb19d9c375c8d886d1c16
David Harris <dharris@2328.lan>
2020-01-21 10:18:37 -0500

    solid bootstrap 4 stack

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -3,10 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 from dashboard.views import dashboard
-
 app.register_blueprint(dashboard.bp)
-
 
 @app.route('/')
 def index():
     return render_template("/dashboard/index.html")
+
